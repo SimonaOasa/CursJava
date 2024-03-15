73aaa761af8500940d2a33c604455512ccb0b66f
simona oasa <simona.oasa@outlook.com>
2024-03-15 16:39:17 +0200

    curs 5 intermediar

---
+++ src/curs5/LetterSearch.java
@@ -0,0 +1,45 @@
+package curs5;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * facem un program care citeste de la tastatura un text 
+ * in text cautam litera 'o'
+ * daca gasim litera printam ca am gasit litera, daca nu 
+ * printam ca nu am gasit litera in text 
+ * 
+ */
+
+public class LetterSearch {
+
+	public static void main(String[] args) {
+	    
+		System.out.println("Please enter a text : ");
+		Scanner scan = new Scanner (System.in);
+		String text = scan.next().toLowerCase();
+		//String text = scan.next();
+		int j = 0; 
+		
+		for (int i=0; i<text.length(); i++) {
+			
+			if(text.charAt(i)=='o' || text.charAt(i)=='O') {
+				//System.out.println("Am gasit litera in text!");
+				//break;
+				j++;
+					
+		}
+
+	}
+
+	/*	if (j!=0 ) {
+			System.out.println("Am gasit de " +j+" ori litera in text!");
+		}else {
+			System.out.println(" Nu am gasit litera in text!");
+		}*/
+		String result = j!=0?"Am gasit de " +j+" ori litera in text!": " Nu Am gasit de " +j+" ori litera in text!";
+		System.out.println(result);
+		
+	}
+}
+
