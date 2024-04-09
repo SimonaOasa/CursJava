bff67cd72d34de46db1dea6ff2fea94ff140bad4
simona oasa <simona.oasa@outlook.com>
2024-04-09 18:08:12 +0300

    curs12 final

--- src/curs12/ExceptionExample4.java
+++ src/curs12/ExceptionExample4.java
@@ -16,40 +16,48 @@ public class ExceptionExample4 {
 		boolean conditieNumar1 = true;
 		boolean conditieNumar2 =true;
 		do {
-			System.out.println("Please enter number 1 : ");
 			try {
-			  //Integer.parseInt()
-			  number1 = Integer.parseInt(scan.next());
-			  
-			}catch(NumberFormatException e) {
-				//e.printStackTrace();
-				System.out.println("Please enter only numeric values!");
-				continue;
-			}
-			
-		do {
-			System.out.println("Please enter number 2 : ");
-			try {
-			  //Integer.parseInt()
-			  number2 = Integer.parseInt(scan.next());
-			  
-			}catch(NumberFormatException e) {
-				//e.printStackTrace();
-				System.out.println("Please enter only numeric values!");
-				continue;
-			}
-			conditieNumar2 = false;;
-		}while(conditieNumar2);
+				System.out.println("Please enter number 1 :");
+				try{
+					//Integer.parseInt()
+					number1 = Integer.parseInt(scan.next());
+					
+				}catch(NumberFormatException e) {
+					//e.printStackTrace();
+					System.out.println("Please enter only numeric values!");
+					continue;
+				}
+				do {
+					System.out.println("Please enter number 2 :");
+					try{
+						//Integer.parseInt()
+						number2 = Integer.parseInt(scan.next());
+						
+					}catch(NumberFormatException e) {
+						//e.printStackTrace();
+						System.out.println("Please enter only numeric values!");
+						continue;
+					}
+					conditieNumar2 = false;
+				}while(conditieNumar2);	
 		
-		if(number1 ==0 || number2 ==0 ) {
-			System.out.println("One of the numbers is 0! Try again!");
-			continue;
-		}
+				if(number1 == 0 || number2 == 0) {
+					//System.out.println("One of the numbers is 0! Try again!");
+					//continue;
+					throw new ArithmeticException("One of the numbers is 0! Try again!");
+				}
+					
+				System.out.println("Result : " + number1/number2);
+				conditieNumar1=false;	
 		
-		System.out.println("Result : " + number1/number2);
-			
-		conditieNumar1 = false;	
-		}while (conditieNumar1);
+			}catch(Exception e) {
+				
+				e.printStackTrace();
+			}
+				
+				
+
+			}while(conditieNumar1);
 		
 	}
 
