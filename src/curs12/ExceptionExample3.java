0c1f919a6c1941f0f194d90c0bb7466eb6bf9ef7
simona oasa <simona.oasa@outlook.com>
2024-04-09 16:36:43 +0300

    curs12 jumatate

---
+++ src/curs12/ExceptionExample3.java
@@ -0,0 +1,24 @@
+package curs12;
+
+public class ExceptionExample3 {
+
+	public static void main(String[] args) {
+		
+		
+		try {
+			String nume = null;
+			System.out.println(nume.length());
+		}catch(Exception e) {
+			System.out.println("Exception occured");
+			System.out.println(3/0);
+			throw new Exception("");
+		}finally {
+			System.out.println("Finally block of code!");
+		}
+		
+		System.out.println("Code after TRY- CATCH");
+		
+
+	}
+
+}
