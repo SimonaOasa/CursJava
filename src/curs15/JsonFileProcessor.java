eeb1f3c897e011345601971e62ff3f57c87fe3b2
simona oasa <simona.oasa@outlook.com>
2024-04-19 16:49:14 +0300

    curs15 intermediar

---
+++ src/curs15/JsonFileProcessor.java
@@ -0,0 +1,61 @@
+package curs15;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+/*
+ * [{ name = "ion" , 
+ *   email = "ion@ion.com"
+ *   },
+ *   { name = "ion",
+ *    email = {
+ *            homeEmail = "ceva",
+ *            workemail = {}, {}
+ *    
+ *             }
+ *    }
+ *     
+ * 
+ * ]
+ */
+public class JsonFileProcessor {
+
+	@SuppressWarnings("unchecked")
+	public void writeJsonFile() {
+		
+		JSONObject obj = new JSONObject();
+		obj.put("name", "Oana");
+		obj.put("email", "oana@oana.net");
+		
+		try(FileWriter file = new FileWriter("test.json")) {
+			
+			file.write(obj.toJSONString());
+			
+		}catch(IOException e) {
+			System.out.println("Nu am putut scrie fisierul");
+		}
+		
+		
+	}
+	
+	
+	public void readJsonFile(String key) {
+		
+		try(FileReader file = new FileReader("test.json")){
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObj = (JSONObject) parser.parse(file);
+			
+			String value = (String) jsonObj.get(key);
+			System.out.println(value);
+			
+		}catch(Exception e ) {
+			System.out.println("Nu am putut scrie fisierul");
+		}
+		
+	}
+	
+	
+}
